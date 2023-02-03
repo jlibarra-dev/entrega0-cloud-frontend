23f650b2abb006c6d6a84ae24be792320544f9c6
Lucas Ibarra <jl.ibarra@uniandes.edu.co>
2023-02-03 01:21:54 -0500

    Login fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useId } from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { ReactSession }  from 'react-client-session';
 
 let incorrectEmail = <p>Incorrect email format. Please change it</p>
 let validEmailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
@@ -12,12 +12,23 @@ let spinner = (<div className="spinner-border text-primary" role="status">
 </div>)
 
 function App() {
-  const idEmail = useId();
+  
+  // ReactSession.set("id", "Bob");
+  // if (ReactSession.get("id") == undefined){
+  //   console.log('No hay usuario logueado')
+  // }
+  // else{
+  //   //return navigate('/events')
+  // }  
+  let navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [correctEmail, setCorrectEmail] = useState(true);
   const [password, setPassword] = useState('');
   const [emptyField, setEmptyField] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const [isLogged, setIsLogged] = useState(!ReactSession.get("id") == undefined);
 
   const handleEmailChange = (event) => {
     // 👇 Get input value from "event"
@@ -42,8 +53,10 @@ function App() {
         // Exito
         .then(response => response.json())  // convertir a json
         .then(json => {
+          setLoading(true)
           if (json.message === 'Correct auth') {
-            console.log('Exito auth')
+            ReactSession.set("id", json.id);
+            return navigate('/events')
           }
           else if (json.message === 'Incorrect password') {
             console.log('Incorrect passwd')
@@ -71,14 +84,15 @@ function App() {
       </div>
       <div><div className="mb-3">
         <label htmlFor="userEmail" className="form-label">Email address:</label>
-        <input type="email" className="form-control" id={idEmail} placeholder="name@example.com" onChange={handleEmailChange} />
+        <input type="email" className="form-control" id='userEmail' placeholder="name@example.com" onChange={handleEmailChange} />
         {(correctEmail ? null : incorrectEmail)}
       </div>
         <div className="mb-3">
           <label htmlFor="exampleFormControlInput1" className="form-label">Password:</label>
           <input type="password" className="form-control" id="userPassword" placeholder="Your Password" onChange={handlePasswordChange} />
         </div>
-        {(setLoading?<button type="button" className="btn btn-primary" onClick={login}>Login</button>:spinner)}
+        {(loading?spinner:<button type="button" className="btn btn-primary" onClick={login}>Login</button>)}
+        <br></br>
         {(emptyField ? emptyFieldText : null)}
         <Link to="/register">Not registered? What are you waiting?</Link></div>
     </>
--- src/EventsPage/EventPage.jsx
+++ src/EventsPage/EventPage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import EvenItem from './EvenItem';
+import { ReactSession }  from 'react-client-session';
 var name = 'Lucas'
 var eventos = [[1, 'Evento 1', 'Conferencia', 'Uniandes', 'Cr. 3', '12-08-2000', '12-08-2022'],
 [2, 'Evento 2', 'Seminario', 'Uniandes Chia', 'Cr. 4', '12-08-2001', '12-08-2022'],
@@ -7,24 +8,24 @@ var eventos = [[1, 'Evento 1', 'Conferencia', 'Uniandes', 'Cr. 3', '12-08-2000',
 [2, 'Evento 2', 'Seminario', 'Uniandes Chia', 'Cr. 4', '12-08-2001', '12-08-2022'],
 [2, 'Evento 2', 'Seminario', 'Uniandes Chia', 'Cr. 4', '12-08-2001', '12-08-2022'],
 [2, 'Evento 2', 'Seminario', 'Uniandes Chia', 'Cr. 4', '12-08-2001', '12-08-2022']]
-class EventPage extends Component {
-    render() {
-        return (
-            <>
-                <div className="App">
-                    <h1>Welcome to the Event Manager {name}!</h1>
-                </div>
-                <div>
-                    {
-                        eventos.map((x) => {
-                            return <EvenItem eventName={x[1]} eventDate={x[5] +  ' to '+ x[6]}></EvenItem>
-                        })
-                    }
-                    
-                </div>
-            </>
-        );
-    }
+
+const EventPage = () => {
+    console.log(ReactSession.get("id"))
+    return (
+        <>
+            <div className="App">
+                <h1>Welcome to the Event Manager {name}!</h1>
+            </div>
+            <div>
+                {
+                    eventos.map((x) => {
+                        return <EvenItem eventName={x[1]} eventDate={x[5] + ' to ' + x[6]}></EvenItem>
+                    })
+                }
+
+            </div>
+        </>
+    );
 }
 
 export default EventPage;
